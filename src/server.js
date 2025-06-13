0e706755ba67993da87960fc4748af8b87e87a7f
mateoeulORT <48044168@est.ort.edu.ar>
2025-06-13 19:04:22 -0300

    gets termminados

--- src/server.js
+++ src/server.js
@@ -17,16 +17,16 @@ app.use(express.json()); // Middleware para parsear y comprender JSON
 // (por ejemplo)
 //
 app.get('/api/alumnos/', async (req, res) => {
-
     const cliente = new Client(config)
     try {
         await cliente.connect()
         const result = await cliente.query("SELECT * FROM ALUMNOS")
         const alumnos = result.rows;
+        //console.log("Alumnos obtenidos:", alumnos);
         res.status(200).json(alumnos);
     } 
     catch (error) {
-        console.error("Error al obtener provincias:", error);
+        console.error("Error al obtener estudiantes:", error);
         res.status(500).json({ error: error.message });
     }
     finally {
@@ -34,7 +34,37 @@ app.get('/api/alumnos/', async (req, res) => {
     }
 })
 
-//app.get('/api/alumnos/:id', async (req, res) => {...})
+app.get('/api/alumnos/:id', async (req, res) => {
+
+    const cliente = new Client(config)
+    const id = req.params.id;
+    if(isNaN(Number(id))){
+        return res.status(400).json({ error: "El ID debe ser un número" });
+    }
+    
+    try {
+        await cliente.connect()
+        const result = await cliente.query("SELECT * FROM ALUMNOS WHERE ID = $1", [id]);
+        const alumno = result.rows[0];
+        
+        //si no encuentra el alumno, devuelve un error 404
+        if (!alumno) {
+            return res.status(404).json({ error: "Estudiante no encontrado" });
+        }
+        //si lo encuentra, devuelve el alumno
+        console.log("Alumno obtenido:", alumno);
+        res.status(200).json(alumno);
+
+    } catch (error) {
+        console.error("Error al obtener el estudiante:", error);
+        res.status(500).json({ error: error.message });
+    }
+    finally{
+        await cliente.end()
+    }
+
+
+})
 //app.post('/api/alumnos/', async (req, res) => {...})
 //app.put('/api/alumnos/', async (req, res) => {...})
 //app.delete('/api/alumnos/:id', async (req, res) => {...})
